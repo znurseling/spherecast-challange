2d424dd0c5d96896dc00a7f81211ff7115078c52
Aral Akus <aral.akus@tum.de>
2026-04-19 10:33:16 +0200

    finalizing

--- agnes/test_sub.py
+++
@@ -1,5 +0,0 @@
-from app.consolidation import find_substitutes, search_by_material
-print("Subs for 'vitamin c':", find_substitutes("vitamin c"))
-print("Search for 'vitamin c':", search_by_material("vitamin c")["count"])
-print("Subs for 'ascorbic acid':", find_substitutes("ascorbic acid"))
-print("Search for 'ascorbic acid':", search_by_material("ascorbic acid")["count"])
